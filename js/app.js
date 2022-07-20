93834e2e36d31bd1e1853c8067c3f03bdd69ff34
Yug0046 <ymcoolguy@gmail.com>
2022-07-20 18:09:52 +0530

    removed connect wallet + edited style

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ window.addEventListener('DOMContentLoaded', () => {
         onboarding.startOnboarding();
       };
     } else if (accounts && accounts.length > 0) {
-      onboardButton.innerText = `✔ ...${accounts[0].slice(-4)}`;
+      onboardButton.innerText = `✔ ${accounts[0].slice(4)}...${accounts[0].slice(-4)}`;
       onboardButton.disabled = true;
       onboarding.stopOnboarding();
       checkOwner(accounts[0]);
@@ -74,60 +74,3 @@ const checkOwner = async (account) => {
   }
 }
 
-function updateStatusText(isOwner, checking) {
-  const statusText = document.querySelector('.owner-status');
-  if(checking) {
-    if(isOwner) {
-      statusText.innerText = `You do own ${COLLECTION_NAME}!! Let's see how many${renderDots(dots)}`;
-    } else {
-      statusText.innerText = `Checking to see if you own any ${COLLECTION_NAME} ${renderDots(dots)}`;
-    }
-  } else {
-    if(isOwner) {
-      statusText.innerText = `You own ${editions.length} ${COLLECTION_NAME}!! `;
-    } else {
-      statusText.innerText = `You don't own any ${COLLECTION_NAME} `;
-    }
-  }
-  dots = dots === 3 ? 1 : dots + 1;
-}
-
-function renderDots(dots) {
-  let dotsString = '';
-  for (let i = 0; i < dots; i++) {
-    dotsString += '.';
-  }
-  return dotsString;
-}
-
-function timer(ms) {
-  return new Promise(res => setTimeout(res, ms));
-}
-
-async function fetchWithRetry(url)  {
-  await timer(TIMEOUT);
-  return new Promise((resolve, reject) => {
-    const fetch_retry = (_url) => {
-      return fetch(_url).then(async (res) => {
-        const status = res.status;
-
-        if(status === 200) {
-          return resolve(res.json());
-        }            
-        else {
-          console.error(`ERROR STATUS: ${status}`)
-          console.log('Retrying')
-          await timer(TIMEOUT)
-          fetch_retry(_url)
-        }            
-      })
-      .catch(async (error) => {  
-        console.error(`CATCH ERROR: ${error}`)  
-        console.log('Retrying')    
-        await timer(TIMEOUT)    
-        fetch_retry(_url)
-      }); 
-    }
-    return fetch_retry(url);
-  });
-}
